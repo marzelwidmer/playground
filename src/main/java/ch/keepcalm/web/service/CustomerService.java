ac04eb8b4f61a804ce3a4de5c0adb06ff7c99163
Marcel Widmer <marzelwidmer@gmail.com>
2016-09-26 11:20:07 +0200

    add travis and circleci add some service customer tests

--- src/main/java/ch/keepcalm/web/service/CustomerService.java
+++ src/main/java/ch/keepcalm/web/service/CustomerService.java
@@ -5,19 +5,36 @@ import ch.keepcalm.web.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by marcelwidmer on 26.09.16.
  */
 @Service
 public class CustomerService {
 
-    private CustomerRepository repostitory;
+    private CustomerRepository repository;
     @Autowired
-    public CustomerService(CustomerRepository repostitory){
-        this.repostitory = repostitory;
+    public CustomerService(CustomerRepository repository){
+        this.repository = repository;
     }
 
-    public Customer createCustomer(Customer model){
-        return repostitory.save(model);
+    public Customer createCustomer(Customer customer){
+        return repository.save(customer);
+    }
+
+    public void deleteCustomer(Customer customer){
+        repository.delete(customer);
+    }
+
+    public Customer getCustomer(Long id) {
+        return repository.findOne(id);
+    }
+
+    public List<Customer> getCustomers() {
+        List<Customer> customers = new ArrayList<Customer>();
+        repository.findAll().forEach(c -> customers.add(c));
+        return customers;
     }
 }
--- src/test/java/ch/keepcalm/web/service/CustomerServiceTest.java
+++ src/test/java/ch/keepcalm/web/service/CustomerServiceTest.java
@@ -22,8 +22,35 @@ public class CustomerServiceTest {
 
     @Test
     public void saveCustomer() {
-        final Customer customerModel = Customer.builder().firstname("Marcel").name("Widmer").build();
-        final Customer customerResult = service.createCustomer(customerModel);
-        Assert.assertEquals("Marcel", customerResult.getFirstname());
+        final Customer customerResult = service.createCustomer(Customer.builder().firstname("John").name("Doe").build());
+        Assert.assertEquals("John", customerResult.getFirstname());
+        // clean db
+        service.getCustomers().forEach(c -> service.deleteCustomer(c));
+    }
+
+    @Test
+    public void saveAndDeleteCustomer() {
+        // create a customer
+        final Customer customerModel = Customer.builder().firstname("Jack").name("Jackson").build();
+        service.createCustomer(customerModel);
+        // delete a customer
+        service.deleteCustomer(customerModel);
+        final Customer customerResult = service.getCustomer(customerModel.getId());
+        Assert.assertNull(customerResult);
+        // clean db
+        service.getCustomers().forEach(c -> service.deleteCustomer(c));
+    }
+
+    @Test
+    public void getCustomers() {
+        // create a customers
+        service.createCustomer(Customer.builder().firstname("Jack").name("Jackson").build());
+        service.createCustomer(Customer.builder().firstname("John").name("Doe").build());
+        // count customers in db
+        Assert.assertEquals(2, service.getCustomers().size());
+        // clean db
+        service.getCustomers().forEach(c -> service.deleteCustomer(c));
+        // count customers in db after cleanUp
+        Assert.assertEquals(0, service.getCustomers().size());
     }
 }
