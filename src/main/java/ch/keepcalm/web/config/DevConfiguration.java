0eec7cddf4bf76946ec7d2ddda417eb3b8752230
Marcel Widmer <marzelwidmer@gmail.com>
2016-09-27 00:11:53 +0200

    add dev profile

--- src/main/java/ch/keepcalm/web/config/DevConfiguration.java
+++ src/main/java/ch/keepcalm/web/config/DevConfiguration.java
@@ -1,13 +1,17 @@
 package ch.keepcalm.web.config;
 
 import ch.keepcalm.web.bootstrap.DatabaseBootstrap;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * Created by marcelwidmer on 26.09.16.
  */
 @Configuration
+@Profile(value = {"dev"})
+@ConditionalOnClass(name = {"org.h2.Driver"})
 public class DevConfiguration {
 
     @Bean
