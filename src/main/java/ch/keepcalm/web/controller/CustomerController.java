ec60c992832c4f9d79a4c4a8fae275de766ee1c2
Marcel Widmer <marzelwidmer@gmail.com>
2016-09-28 08:41:22 +0200

    add thymleaf ModeViewController with bootstrap

--- src/main/java/ch/keepcalm/web/controller/CustomerController.java
+++ src/main/java/ch/keepcalm/web/controller/CustomerController.java
@@ -3,53 +3,30 @@ package ch.keepcalm.web.controller;
 import ch.keepcalm.web.model.Customer;
 import ch.keepcalm.web.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Created by marcelwidmer on 26.09.16.
  */
-@RestController
-@RequestMapping(value = "/api/customers", produces = "application/hal+json")
+@Controller
 public class CustomerController {
 
 
     CustomerService service;
-
     @Autowired
     public CustomerController(CustomerService service){
         this.service = service;
     }
 
-   /* @GetMapping
-    public ResponseEntity getCustomers() {
-        return new ResponseEntity("customers")
-                .addObject("customers", repository.findAll())
-                .addObject("createLink", linkTo(
-                        methodOn(CustomerController.class).createCustomer(null, null))
-                        .withRel("Create")
-                );
-    }*/
-   @GetMapping
-   public ResponseEntity getCustomers() {
-      /* final ModelAndView modelAndView = new ModelAndView("customers")
-               .addObject("customers", service.getCustomers())
-               .addObject("createLink", linkTo(
-                       methodOn(CustomerController.class).createCustomer(null))
-                       .withRel("Create")
-               );*/
-
-       return new ResponseEntity<>(service.getCustomers(), HttpStatus.FOUND);
-   }
-    @PostMapping
-    public ResponseEntity createCustomer(@RequestBody Customer customer) {
-        final Customer result = service.createCustomer(customer);
-        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    @GetMapping(value = "customers")
+    public ModelAndView getCustomers(){
+        final List<Customer> customers = service.getCustomers();
+        return new ModelAndView("customers")
+                .addObject("customers",  customers);
     }
 
 }
