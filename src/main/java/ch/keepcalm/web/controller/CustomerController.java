d3fb9fe8472d08a8271185f54689e8ee1b6881ff
Marcel Widmer <marzelwidmer@gmail.com>
2016-09-29 08:59:44 +0200

    add create customers with HATOAS thymleaf client

--- src/main/java/ch/keepcalm/web/controller/CustomerController.java
+++ src/main/java/ch/keepcalm/web/controller/CustomerController.java
@@ -4,10 +4,12 @@ import ch.keepcalm.web.model.Customer;
 import ch.keepcalm.web.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 /**
  * Created by marcelwidmer on 26.09.16.
@@ -17,16 +19,40 @@ public class CustomerController {
 
 
     CustomerService service;
+
     @Autowired
-    public CustomerController(CustomerService service){
+    public CustomerController(CustomerService service) {
         this.service = service;
     }
 
-    @GetMapping(value = "customers")
-    public ModelAndView getCustomers(){
-        final List<Customer> customers = service.getCustomers();
+
+    // FIXME: 29.09.16 https://github.com/spring-projects/spring-hateoas/issues/471
+    // @GetMapping(value = "/customers")
+    @RequestMapping(value = "/customers", method = RequestMethod.GET)
+    public ModelAndView getCustomers() {
         return new ModelAndView("customers")
-                .addObject("customers",  customers);
+                .addObject("customers", service.getCustomers())
+                .addObject("createLink", linkTo(
+                        methodOn(CustomerController.class).createCustomer(null, null))
+                        .withRel("Create")
+                );
+    }
+
+
+    // FIXME: 29.09.16 https://github.com/spring-projects/spring-hateoas/issues/471
+    //@PostMapping(value = "/customers/create")
+    @RequestMapping(value = "/customers/create", method = RequestMethod.POST)
+    public ModelAndView createCustomer(String firstname, String lastname) {
+        Customer customer = new Customer();
+        customer.setFirstname(firstname);
+        customer.setLastname(lastname);
+        service.createCustomer(customer);
+
+        return new ModelAndView("created-customer")
+                .addObject("overview", linkTo(
+                        methodOn(CustomerController.class).getCustomers())
+                        .withRel("Overview")
+                );
     }
 
 }
