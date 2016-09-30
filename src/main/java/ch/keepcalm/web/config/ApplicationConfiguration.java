81509713f6d463c54dfa66582f86ee9339c702df
Marcel Widmer <marzelwidmer@gmail.com>
2016-09-30 13:27:13 +0200

    add some i18n texts and add configuration for it.

---
+++ src/main/java/ch/keepcalm/web/config/ApplicationConfiguration.java
@@ -0,0 +1,34 @@
+package ch.keepcalm.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+/**
+ * Created by marcelwidmer on 21/03/16.
+ */
+@Configuration
+public class ApplicationConfiguration extends WebMvcConfigurerAdapter {
+
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new CookieLocaleResolver();
+    }
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+}
